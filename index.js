802e2b595c31b094724cb1969ab19d7fce14621c
Migueldeve <107196966+Migueldeve@users.noreply.github.com>
2023-02-12 15:08:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var preguntauno = prompt("numero 1");
+var preguntados = prompt("numero 2");
+
+var resultado = preguntauno*preguntados;
+
+document.getElementById("id1");
+id1.innerHTML = resultado;
